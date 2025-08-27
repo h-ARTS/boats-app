e556411429b6e6a0544c560f0b90ef4579559a62
Hanan Mufti <hi5hanan@yahoo.com>
2025-08-27 21:04:12 +0200

    feat(auth): add additional exception handler for invalid data on login

--- boats-api/src/main/java/com/hanankhan/boatsapi/auth/exception/AuthExceptionHandler.java
+++ boats-api/src/main/java/com/hanankhan/boatsapi/auth/exception/AuthExceptionHandler.java
@@ -2,14 +2,19 @@ package com.hanankhan.boatsapi.auth.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AuthExceptionHandler {
     @ExceptionHandler(InvalidCredentialsException.class)
-        public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
-        }
+    public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 }
